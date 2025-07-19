73b5577b9e223bf35dc0192391a90da6efa69979
Mauricio Ojeda <mauricioojeda@MacBook-Air-de-Mauricio.local>
2025-07-19 13:34:49 -0400

    tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, CoursesModule],
 })
 export class AppModule {}
-
-
-
---
+++ src/courses/dto/create-course.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsUrl()
+  @IsOptional()
+  imageUrl?: string;
+}
